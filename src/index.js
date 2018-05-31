3b849574cc17e11d4ad4f88519a8e26a9c91d5f6
Hai Nguyen <hai5nguy@users.noreply.github.com>
2018-05-31 19:53:34 -0400

    Add api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
+import 'babel-polyfill';
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./_Styles/Index.scss";
 import Logo from './Components/logo';
+import Test from './Components/test';
 
+import KoalaTApi from './Actions/koalat-api';
+
+const api = new KoalaTApi();
+
+// api.sendGraph({
+//     query: `query { listUsers { items { id email }}}`
+// }).then(d => {
+//     console.log('data', d)
+// })
+
+// // console.log('data', data);
+
+api.sendQuery(`{listUsers{items{id email alsdjfds sdlf sljfdsj fsldf sdljf sdflsd fjlsdjfsdlf sdlfj sdlfj }}}`)
 
 class App extends React.Component {
     render() {
         return (
             <div>
               <Logo />
+                <Test />
             </div>
         )
     }
