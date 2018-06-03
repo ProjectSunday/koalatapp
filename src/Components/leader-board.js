2ea2e0ebb00ccc17acc753bb9c52a44358aba778
James Boyer <jamesboyer@Jamess-MacBook-Pro.local>
2018-06-03 11:39:42 -0400

    added more styling to leader board app

--- src/Components/leader-board.js
+++ src/Components/leader-board.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Svg from '../_Styles/Imgs/Koala.svg';
 import '../_Styles/leader-board.scss';
 
 class LeaderBoard extends React.Component {
     render() {
 
-        const title = "FOOBAR";
+        const title = "Koala-T ";
         const info = "stats";
 
         return(
             <div className="leader-board-box">
-              <div className="title"> {title} </div>
+              <div className="title"> <img src={Svg} /> <h1>{title}  Leader Board</h1> </div>
               <div className="board-row">
                 {info}
               </div>
--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ class App extends React.Component {
         return (
             <div>
               <LeaderBoard />
-             {/*   <PlayerView /> */}
+               {/* <PlayerView />  */}
               {/* <Test /> */}
             </div>
         )
