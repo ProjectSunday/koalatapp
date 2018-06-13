54cbfe6f1df4701b3c14ccd8e6fda9f4fe41ede6
Hai Nguyen <hai5nguy@users.noreply.github.com>
2018-06-13 18:25:35 -0400

    Added UserSignup

---
+++ src/Components/UserSignup/UserSignup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './user-signup.scss';
+
+// import sigupUser from '../Actions/user-actions'
+
+class UserSignup extends React.Component {
+    render() {
+        return (
+            <div>
+                i am UserSignup
+            </div>
+        );
+    }
+}
+
+export default UserSignup;
