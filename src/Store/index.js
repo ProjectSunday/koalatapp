f688979c03701eadc36e438a413b95693d4e607c
Hai Nguyen <hai5nguy@users.noreply.github.com>
2018-06-23 11:31:33 -0400

    added connect

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,12 +1,20 @@
-function test1(state = {}, action) {
+function test1(state = { test: 'this is a test aaaa' }, action) {
     switch(action.type) {
         case 'TEST_1':
           return {
               test: action.testValue
           }
+        default:
+          return state;
     }
 }
 
 import { combineReducers, createStore } from 'redux';
-const reducer = combineReducers({test1});
-const store = createStore(reducer);
+const reducer = combineReducers({ testing: test1 });
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+export default store;
