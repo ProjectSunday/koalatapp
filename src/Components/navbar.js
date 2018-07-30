54e4c8eb09307acb6b3ee95cc8b3fd66755e4d92
jamesboyer92 <jamesboyer92@gmail.com>
2018-07-30 19:56:58 -0400

    added material-ui, started working on navbar styling with mui

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import {
+    AppBar, Toolbar, Button, Typography,
+} from '@material-ui/core';
 import '../_Styles/navbar.scss';
 
 class Navbar extends React.Component {
     render() {
         if (this.props.user.role === 'user') {
             return (
-                <div className="navbar">
-                    <ul>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="title" color="inherit" style={{ flex: 1 }}>
+                          Koala-T
+                        </Typography>
+                        <Button color="inherit"><a href="https://koalauserpool-dev.auth.us-east-2.amazoncognito.com/login?response_type=code&client_id=ncfrp1i43pfh1av4blr7pda8r&redirect_uri=http://localhost:8080/authcallback">Login</a></Button>
+                        {/* <ul>
                         <li>
                             <Link to="/UserHome">Home</Link>
                         </li>
                         <li>
                             <Link to="/userprofile"> Profile</Link>
                         </li>
-                    </ul>
-                </div>
+                    </ul> */}
+                    </Toolbar>
+                </AppBar>
             );
         }
 
