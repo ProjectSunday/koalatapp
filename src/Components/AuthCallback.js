1577406bcf57b2b72b86191edff1f0ba09ff50f0
jamesboyer92 <jamesboyer92@gmail.com>
2018-08-02 21:29:40 -0400

    added loading circular progress bar to authcallback

--- src/Components/AuthCallback.js
+++ src/Components/AuthCallback.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 import { AuthActions } from 'Actions';
+import { withStyles, CircularProgress, purple } from '@material-ui/core';
+import '../_Styles/loader.scss';
 
-export default class AuthCallback extends React.Component {
-    constructor() {
-        super();
+const styles = theme => ({
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
+});
+
+class AuthCallback extends React.Component {
+    constructor(props) {
+        super(props);
         this.state = { authenticated: false };
         const h = window.location.href;
         AuthActions.authenticate(h).then(() => {
@@ -17,6 +25,12 @@ export default class AuthCallback extends React.Component {
 
     render() {
         if (this.state.authenticated) return <Redirect to="/" />;
-        return <div>Authenticating...</div>;
+        return (
+            <div className="loader">
+                <CircularProgress className={styles.progress} size={200} style={{ margin: 'auto' }} />
+            </div>
+        );
     }
 }
+
+export default withStyles(styles)(AuthCallback);
