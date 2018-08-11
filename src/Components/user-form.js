a29f51fff4f98c989e1a6e4ce5155536210e291c
jamesboyer92 <jamesboyer92@gmail.com>
2018-08-11 11:19:40 -0400

    fixed user profile, styling so its no longer touching the navbar, fixed edit form

--- src/Components/user-form.js
+++ src/Components/user-form.js
@@ -1,55 +1,63 @@
 import React from 'react';
 import TextInput from './text-input';
 import '_Styles/user-form.scss';
-import James from '_Styles/Imgs/james.jpg';
+import { Avatar, IconButton, MoreVertIcon, Card, CardHeader, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
-const UserForm = (props) => 
-            <div className="edit-user-form">
-                <div className="edit-form-content">
-                    <div className="head" />
-                        <img src={ James } />
-                    <div className="form-container">
-                    <form>
-                         <TextInput
-                            name="firstName"
-                            label="First Name"
-                            value={props.user.firstName}
-                            onChange={props.onChange}
-                        />
+const UserForm = (props) =>
+  <div className="user-form-container">
+    <Card style={{ borderRadius: '0' }}>
+        <CardHeader
+            avatar={(
+                <Avatar>J</Avatar>
+            )}
+            title={props.givenName}
+            style={{ fontFamily: 'Julius Sans One' }}
+        />
+        <CardMedia
+            style={{ height: 0, paddingTop: '56.25%', borderRadius: '100' }}
+            image={props.user.img}
+        />
+        <CardContent>
+        <form>
+                <TextInput
+                name="firstName"
+                label="First Name"
+                value={props.user.givenName}
+                onChange={props.onChange}
+            />
 
-                        <TextInput
-                            name="lastName"
-                            label="Last Name"
-                            value={props.user.lastName}
-                            onChange={props.onChange}
-                        />
+            <TextInput
+                name="lastName"
+                label="Last Name"
+                value={props.user.familyName}
+                onChange={props.onChange}
+            />
 
-                        <TextInput
-                            name="img"
-                            label="Img"
-                            value={props.user.img}
-                            onChange={props.onChange}
-                        />
+            <TextInput
+                name="img"
+                label="Img"
+                value={props.user.imgUrl}
+                onChange={props.onChange}
+            />
 
-                        <TextInput
-                            name="email"
-                            label="Email"
-                            value={props.user.email}
-                            onChange={props.onChange}
-                        />
+            <TextInput
+                name="email"
+                label="Email"
+                value={props.user.email}
+                onChange={props.onChange}
+            />
 
-                        <input
-                            type="submit"
-                            disabled={props.saving}
-                            className="btn btn-primary"
-                            onClick={(event)=> props.onSave, props.toggleEdit}
-                        />
+            <input
+                type="submit"
+                disabled={props.saving}
+                className="btn btn-primary"
+                onClick={(event)=> props.onSave, props.toggleEdit}
+            />
+        </form>   
+        </CardContent>
+    </Card>
 
-                    </form>
-                    </div>
-                </div>
-            </div>
-
-
+  </div>
+                
 
 export default UserForm;
